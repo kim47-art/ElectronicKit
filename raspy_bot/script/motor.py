78a2b6c212a987115b758f1ba5027af559192a6a
kim47-art <jojo.oramuda@gmail.com>
2020-07-25 18:35:55 +0900

    ros package化

--- raspy_bot/script/motor.py
+++ raspy_bot/script/motor.py
@@ -4,31 +4,32 @@
 import pigpio
 import time
 
-motor_A=17
-motor_B=14
-motor_C=27
-motor_D=23
 
-motor_1=4
-motor_2=15
-motor_3=18
-motor_4=22
 
 #gpio_pin
 MOTOR_PARAM = [
-     {"gpio_pin_no":4, "pwm_max":1900, "pwm_min":1400,"cw":True},#1
+     {"gpio_pin_no":4, "pwm_max":1800, "pwm_min":1400,"cw":True},#1
      {"gpio_pin_no":14, "pwm_max":1750, "pwm_min":1000,"cw":True},#B
      {"gpio_pin_no":15, "pwm_max":2000, "pwm_min":1300,"cw":False},#2
      {"gpio_pin_no":17, "pwm_max":1000, "pwm_min":600,"cw":False},#A
      {"gpio_pin_no":18, "pwm_max":1600, "pwm_min":750,"cw":True},#3
-     {"gpio_pin_no":22, "pwm_max":1000, "pwm_min":600,"cw":False},#4
-     {"gpio_pin_no":23, "pwm_max":2300, "pwm_min":1900,"cw":True},#D
+     {"gpio_pin_no":22, "pwm_max":1000, "pwm_min":550,"cw":False},#4
+     {"gpio_pin_no":23, "pwm_max":2350, "pwm_min":1900,"cw":True},#D
      {"gpio_pin_no":27, "pwm_max":2250, "pwm_min":1300,"cw":False},#C
 ]
 
 
 #真横が0度、下が90度
 class Motor:
+     motor_A=17
+     motor_B=14
+     motor_C=27
+     motor_D=23
+
+     motor_1=4
+     motor_2=15
+     motor_3=18
+     motor_4=22
      def __init__(self,pin):
           self.pi = pigpio.pi()
           self.pin=pin
---
+++ raspy_bot/script/walk.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+ 
+import pigpio
+import time
+import motor
+
+#def stand_up():
+
+front_left_knee=motor.Motor(motor.Motor.motor_B)
+front_left_leg=motor.Motor(motor.Motor.motor_A)
+rear_left_knee=motor.Motor(motor.Motor.motor_C)
+rear_left_leg=motor.Motor(motor.Motor.motor_D)
+
+front_right_knee=motor.Motor(motor.Motor.motor_2)
+front_right_leg=motor.Motor(motor.Motor.motor_1)
+rear_right_knee=motor.Motor(motor.Motor.motor_3)
+rear_right_leg=motor.Motor(motor.Motor.motor_4)
+
+front_left_leg.sync_rotate(0)
+front_left_knee.sync_rotate(90)
+rear_left_leg.sync_rotate(0)
+rear_left_knee.sync_rotate(90)
+front_right_leg.sync_rotate(0)
+front_right_knee.sync_rotate(90)
+rear_right_leg.sync_rotate(0)
+rear_right_knee.sync_rotate(90)
+
+
+
+for i in range(5):
+
+    #step1
+    front_left_leg.sync_rotate(70)
+    front_left_knee.sync_rotate(90)
+    rear_left_leg.sync_rotate(70)
+    rear_left_knee.sync_rotate(90)
+    front_right_leg.sync_rotate(0)
+    front_right_knee.sync_rotate(90)
+    rear_right_leg.sync_rotate(0)
+    rear_right_knee.sync_rotate(90)
+    time.sleep(0.5)    
+
+    #break
+
+    #step2 
+    front_right_knee.sync_rotate(60)
+    time.sleep(0.1)
+    front_right_leg.sync_rotate(80)
+    time.sleep(0.1)
+    #front_right_knee.sync_rotate(90)  
+    #time.sleep(1)
+
+
+    #move
+    front_left_leg.sync_rotate(0)
+    front_right_leg.sync_rotate(30)
+    front_right_knee.sync_rotate(90)
+    rear_left_leg.sync_rotate(90)
+    rear_right_leg.sync_rotate(80)
+    time.sleep(0.5)
+
+
+    #step3
+    rear_left_knee.sync_rotate(60)
+    time.sleep(0.1)
+    rear_left_leg.sync_rotate(0)
+    time.sleep(0.1)
+    rear_left_knee.sync_rotate(90)
+    time.sleep(0.5)
+
+
+    #step4
+    front_left_knee.sync_rotate(60)
+    time.sleep(0.1)
+    front_left_leg.sync_rotate(90)
+    time.sleep(0.1)
+    #front_left_knee.sync_rotate(90)
+    #time.sleep(1)
+
+
+    #move
+    front_right_leg.sync_rotate(0)
+    front_left_leg.sync_rotate(30)
+    front_left_knee.sync_rotate(90)
+    rear_right_leg.sync_rotate(90)
+    rear_left_leg.sync_rotate(60)
+    time.sleep(0.5)    
+
+    #move->step1
+    rear_right_knee.sync_rotate(60)
+    time.sleep(0.1)
+    rear_right_leg.sync_rotate(90)
+    time.sleep(0.1)
+
+front_left_leg.sync_rotate(0)
+front_left_knee.sync_rotate(90)
+rear_left_leg.sync_rotate(0)
+rear_left_knee.sync_rotate(90)
+front_right_leg.sync_rotate(0)
+front_right_knee.sync_rotate(90)
+rear_right_leg.sync_rotate(0)
+rear_right_knee.sync_rotate(90)
